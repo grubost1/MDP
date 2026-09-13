9f8b48b81a6bcb28238cc40a353dcbe12a3ab3c0
shpatell <pospeloff2018@yandex.ru>
2026-09-13 23:10:59 +0700

    Написана верстка и базовая структура приложения

--- app/src/main/kotlin/com/example/beetles/MainActivity.kt
+++ app/src/main/kotlin/com/example/beetles/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.example.beetles
 
 import android.os.Bundle
+import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+data class Player(
+    val fullName: String,
+    val gender: String,
+    val course: String,
+    val difficulty: Int,
+    val birthDate: String,
+    val zodiacSign: String
+)
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etFullName: EditText
+    private lateinit var rgGender: RadioGroup
+    private lateinit var spinnerCourse: Spinner
+    private lateinit var seekBarDifficulty: SeekBar
+    private lateinit var calendarView: CalendarView
+    private lateinit var btnSubmit: Button
+    private lateinit var tvResult: TextView
+    private lateinit var ivZodiac: ImageView
+    private lateinit var tvDifficultyValue: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        etFullName = findViewById(R.id.etFullName)
+        rgGender = findViewById(R.id.rgGender)
+        spinnerCourse = findViewById(R.id.spinnerCourse)
+        seekBarDifficulty = findViewById(R.id.seekBarDifficulty)
+        calendarView = findViewById(R.id.calendarView)
+        btnSubmit = findViewById(R.id.btnSubmit)
+        tvResult = findViewById(R.id.tvResult)
+        ivZodiac = findViewById(R.id.ivZodiac)
+
+        tvDifficultyValue = findViewById(R.id.tvDifficultyValue)
+
+        val courses = arrayOf("1 курс", "2 курс", "3 курс", "4 курс", "Магистратура")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, courses)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerCourse.adapter = adapter
     }
 }
